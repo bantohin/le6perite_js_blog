44a062a51a610093d45c844cc395470e3489b9e2
DanailIliew <danaililiew@gmail.com>
2016-12-02 22:51:02 +0200

    dan4o 1.1

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+sessionStorage.setItem('authToken', btoa("guest:guest"));
 
+(function app() {
+    let router = Sammy(function () {
+        let baseUrl = "https://baas.kinvey.com/";
+        let appId = "kid_rygdnrymg";
+        let appSecret = "62388f7eecea488780b40e25928ef190";
+
+        let authService = new AuthService(appId, appSecret);
+    })
+})();
