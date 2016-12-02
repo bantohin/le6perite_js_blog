ebab6cb23869de8b9733da0eeefabf561a3cab08
bantohin <dimitartachev23@gmail.com>
2016-12-03 00:27:35 +0200

    Mitio 1.0

--- scripts/controllers/postController.js
+++ scripts/controllers/postController.js
@@ -1,1 +1,38 @@
+class PostController {
+    constructor(view, model) {
+        this.model = model;
+        this.view = view;
+    }
 
+    listPosts() {
+        this.model.getPosts()
+            .then(function (successData) {
+                this.view.showPosts(successData);
+            })
+            .catch(function (errorData) {
+                //TODO: fill notification
+            });
+    }
+
+    viewPost(id) {
+        this.model.getPost()
+            .then(function (successData) {
+                this.view.showPost(successData);
+            })
+            .catch(function (errorData) {
+                //TODO: fill notification
+            });
+    }
+
+    createPost(data) {
+        //TODO...
+    }
+
+    editPost(id, data) {
+        //TODO...
+    }
+
+    deletePost(id) {
+        //TODO...
+    }
+}
---
+++ scripts/views/postView.js
@@ -0,0 +1,25 @@
+class PostView {
+    constructor(controller) {
+        this.controller = controller;
+    }
+
+    showPosts(data) {
+        //TODO: render html
+    }
+
+    showPost(data) {
+        //TODO: render html
+    }
+
+    createPost() {
+        //TODO...
+    }
+
+    editPost() {
+        //TODO...
+    }
+
+    deletePost() {
+        //TODO...
+    }
+}
