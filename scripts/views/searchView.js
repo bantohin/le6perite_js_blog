6ad6811740d22930831618c0d1e7662584a42eff
bantohin <dimitartachev23@gmail.com>
2016-12-07 02:30:23 +0200

    Mitio tursachkata

--- scripts/views/searchView.js
+++ scripts/views/searchView.js
@@ -1,7 +1,19 @@
 class SearchView {
     constructor() {
-
     }
 
-
+    displayPostTitles(data) {
+        $('#app').empty();
+        console.log(data);
+        $.get('templates/searchResults-template.html', function (template) {
+            let renderedHtml = Mustache.render(template);
+            $('#app').html(renderedHtml);
+            for(let each of data) {
+                let postTitleDiv = $('<div>');
+                postTitleDiv.appendTo($('#postTitles'));
+                let postTitleLink = $(`<a href="#/posts/details/${each._id}">`).text(each.title);
+                postTitleLink.appendTo(postTitleDiv);
+            }
+        })
+    }
 }
