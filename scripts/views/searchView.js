766fb1eeaa86fc7aafc70e21fb3900b734e6305f
bantohin <dimitartachev23@gmail.com>
2016-12-07 03:19:27 +0200

    Mitio 2.0
    search -> if nothing found -> notify user

--- scripts/views/searchView.js
+++ scripts/views/searchView.js
@@ -8,11 +8,16 @@ class SearchView {
         $.get('templates/searchResults-template.html', function (template) {
             let renderedHtml = Mustache.render(template);
             $('#app').html(renderedHtml);
-            for(let each of data) {
-                let postTitleDiv = $('<div>');
-                postTitleDiv.appendTo($('#postTitles'));
-                let postTitleLink = $(`<a href="#/posts/details/${each._id}">`).text(each.title);
-                postTitleLink.appendTo(postTitleDiv);
+            if(data.length == 0) {
+                $('#postTitles').append($('<span>').text('Sorry, nothing was found. Try again with different keywords.'))
+            }
+            else {
+                for(let each of data) {
+                    let postTitleDiv = $('<div>');
+                    postTitleDiv.appendTo($('#postTitles'));
+                    let postTitleLink = $(`<a href="#/posts/details/${each._id}">`).text(each.title);
+                    postTitleLink.appendTo(postTitleDiv);
+                }
             }
         })
     }
